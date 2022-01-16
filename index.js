3e3fd67928dc3bd5736ce470fc2561a164c3201b
unlimited1974@aol.com <unlimited1974@aol.com>
2022-01-15 18:48:57 -0600

    commit tree edd2f12360f34cc40487186c5fbddb267cf53776

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function addingEventListener() {
     const input = document.getElementById('input');
 
 function clickAlert() {
-  alert('I was clicked!');
+  alert('I was clicked again!');
 }
 input.addEventListener('click', clickAlert);
 }
 addingEventListener()
+// 'Here I go again LOL'
